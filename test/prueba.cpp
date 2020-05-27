5690fb2d38f0e0314632a4603401f44b87dc2a80
diegogarromolina <diego.garromolina@ucr.ac.cr>
2020-05-26 22:49:58 -0600

    Se crea la carpeta de testing

---
+++ test/prueba.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hola desde archivo C++" << endl;
+
+    return 0;
+}
---
+++ test/prueba.sh
@@ -0,0 +1,8 @@
+
+echo "Esto es una prueba"
+
+python3 prueba.py
+
+./prueba
+
+echo "Fin de prueba"
